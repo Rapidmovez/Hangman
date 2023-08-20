d8bf10cc564646566864e7bd0e4385c4d5c2c5c1
Rapidmovez <41858990+Rapidmovez@users.noreply.github.com>
2023-08-20 06:42:20 +0300

    Update Hangman.cpp

--- Hangman.cpp
+++ Hangman.cpp
@@ -1,2 +1,19 @@
 //A hangman game, written in C++.
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "hangman.txt"
+using namespace std;
+int main(){
+  //generate a random word from the "hangman.txt" file
+  //note: all word from the text file will contain only lowercase letters
+  string word;
+  //the number of tries the player has
+  int tries = 0;
+  char letter;
+  //prompt the player for a letter
+  cout << "Choose a letter: ";
+  cin >> letter;
+  
+  return 0;
+}
